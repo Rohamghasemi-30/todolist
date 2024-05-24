7757bc31f6f973d93f40e69513d26c443bf98061
Rohamghasemi-30 <solo.rm30@gmail.com>
2024-05-24 10:48:41 +0330

    finished

--- js/app.js
+++ js/app.js
@@ -1,46 +1,25 @@
-let formTodoList = document.querySelector(".formTodoList");
-let todoList = document.querySelector(".todolist")
-let todolists = [];
+let addButton = document.getElementById('addButton');
+let newTodo = document.getElementById('newTodo');
+let todoList = document.getElementById('todoList');
 
-formTodoList.addEventListener("submit", (e)=>{
-const newTodo = {
-    title: e.target.title.value,
-    isFinished: false,
-    id: Math.random(),
-};
-todolists.push(newTodo);
+document.addEventListener('click', function() {
+    
+addButton.addEventListener('click', function() { 
+if (newTodo.value !== '') { 
+let listItem = document.createElement('li'); 
+listItem.textContent = newTodo.value;
+listItem.classList.add('mb-2', 'flex', 'justify-between', 'items-center');
 
-
-showtodolistsINUI(todolists);
-
-e.preventDefault();
-
-});
-
-function showtodolistsINUI(Todo){
-    Todo.forEach(item => {
-        let div = document.createElement("div");
-        div.classList.add(
-        "list",
-        "w-[90%]",
-        "h-[80px]",
-        "bg-green-200",
-        " mb-2",
-        "rounded-[20px]",
-        "flex",
-        "justify-between",
-        "items-center", 
-        "p-[15px]",
-        )
-div.setAttribute("id", `${item.id}`);
-div.innerHTML = `<p class="title">${item.title}</p>`;
-let btnsDiv = document.createElement("div")
-let deleteBtn = document.createElement("button");
-deleteBtn.classList.add();
-btnsDiv.appendChild(checkBtn);
-btnsDiv.appendChild(deleteBtn);
-div.appendChild(btnsDiv);
-
-todolist.appendChild("div")
-    });
+const deleteButton = document.createElement('button');
+deleteButton.textContent = 'Delete';
+deleteButton.classList.add('bg-red-500', 'hover:bg-red-700', 'text-white', 'font-bold', 'py-1', 'px-2', 'rounded');
+// Help from the Internet
+deleteButton.addEventListener('click', function() {
+listItem.remove();
+  });
+  // Help from the Internet
+listItem.appendChild(deleteButton);
+todoList.appendChild(listItem);
 }
+});
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,6 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
-    colors: {
-      'main': '#ACE1AF',
-      'green-100': '#E0FBE2',
-      'green-200': '#BFF6C3',
-      'green-300': '#B0EBB4',
-      'parent': '#ffffff00',
-    },
   },
   plugins: [],
 }
